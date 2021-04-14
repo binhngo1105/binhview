7931901713fac90b6728bf1c07f0fa30412ecae9
binhngo1105 <81561723+binhngo1105@users.noreply.github.com>
2021-04-14 16:26:17 +0700

    Create addacc.php

---
+++ addacc.php
@@ -0,0 +1,13 @@
+<?php 
+  if (isset($_POST['add'])){ 
+    $user = $_POST['user'];
+    $password= $_POST['password'];
+    $sql = " insert into login values ($user, $password)";
+    $q = pg_query($dbconn, $sql);
+    if($q){
+      header('location: index.php');
+    }
+    else {
+     echo " Loi "   ;
+    }
+?>
